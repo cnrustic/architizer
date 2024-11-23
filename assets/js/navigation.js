120476130f36a8334cc69af4001f2c98fe96a6e1
RyotaHan <42806322+cnrustic@users.noreply.github.com>
2024-11-23 17:10:45 +0800

    new add

---
+++ assets/js/navigation.js
@@ -0,0 +1,44 @@
+/**
+ * File navigation.js.
+ *
+ * Handles toggling the navigation menu for small screens and enables TAB key
+ * navigation support for dropdown menus.
+ */
+( function() {
+    const siteNavigation = document.getElementById( 'site-navigation' );
+
+    // Return early if the navigation doesn't exist.
+    if ( ! siteNavigation ) {
+        return;
+    }
+
+    const button = siteNavigation.getElementsByTagName( 'button' )[ 0 ];
+
+    // Return early if the button doesn't exist.
+    if ( 'undefined' === typeof button ) {
+        return;
+    }
+
+    const menu = siteNavigation.getElementsByTagName( 'ul' )[ 0 ];
+
+    // Hide menu toggle button if menu is empty and return early.
+    if ( 'undefined' === typeof menu ) {
+        button.style.display = 'none';
+        return;
+    }
+
+    if ( ! menu.classList.contains( 'nav-menu' ) ) {
+        menu.classList.add( 'nav-menu' );
+    }
+
+    // Toggle the .toggled class and the aria-expanded value each time the button is clicked.
+    button.addEventListener( 'click', function() {
+        siteNavigation.classList.toggle( 'toggled' );
+
+        if ( button.getAttribute( 'aria-expanded' ) === 'true' ) {
+            button.setAttribute( 'aria-expanded', 'false' );
+        } else {
+            button.setAttribute( 'aria-expanded', 'true' );
+        }
+    } );
+} )(); 
